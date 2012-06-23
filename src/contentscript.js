04526b0b0d9162fe59e7a638d3989086d1f949f0
Matt Atkins <matt@yoomee.com>
2012-06-23 10:39:03 +0100

    Add support for manybooks.net and move button creation into function.

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,21 +1,35 @@
 // If Send to Readmill isn't already implemented
 if (typeof (Readmill) === "undefined") {
+  // Define function to create button
+  function readmillButton(url){
+    var btn = $('<div/>', {
+      'class': 'send-to-readmill',
+      'data-display': 'small',
+      'data-download-url': url,
+      'data-buy-url': url
+    });
+    return $('<div />',{'class': 'send-to-readmill-wrapper'}).html(btn);
+  }
+  var loadReadmillJs = false;
   // If there any links to ePUB files
   var links = $('a:regex(href,\\.epub([\\.\\?]|$))');
   if (links.length > 0) {
     // Add Send to Readmill anchors
     $(links).each(function (index, link) {
-      var btn = $('<div/>', {
-        'class': 'send-to-readmill',
-        'data-display': 'small',
-        'data-download-url': link.href,
-        'data-buy-url': link.href
-      });
-      // Create wrapper div
-      var wrapper = $('<div />',{'class': 'send-to-readmill-wrapper'});
-      wrapper.html(btn)
-      $(link).after(wrapper);
+      $(link).after(readmillButton(link.href));
     });
+    loadReadmillJs = true;
+  }
+  // If on manybooks.net
+  if (/^https?:\/\/.*manybooks\.net\//.test(document.location.href)){
+    var tid = $('input[name="tid"]').val();
+    if (tid && $('select[name="book"]').find('option[value$="epub"]').length){
+      var url = "http://s3.amazonaws.com/manybooksepub_new/" + tid + "epub.epub";
+      $('#submit_button').after(readmillButton(url));
+      loadReadmillJs = true;
+    }
+  }
+  if(loadReadmillJs){
     // Load Send to Readmill script
     (function() {
       var st = document.createElement('script'); st.type = 'text/javascript'; st.async = true;
